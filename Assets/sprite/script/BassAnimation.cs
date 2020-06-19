039fd5ef0bf18444931f0b56ceb35ae11b02ac02
garebareDA <garedaiki@gmail.com>
2020-06-20 02:54:21 +0900

    add:click

---
+++ Assets/sprite/script/BassAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BassAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject obj = getClickObject();
+        if (obj != null)
+        {
+            Debug.Log(obj);
+        }
+    }
+
+    private GameObject getClickObject()
+    {
+        GameObject clickedGameObject = null;
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+
+        return clickedGameObject;
+    }
+}
